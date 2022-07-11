e9adb46ec95e1172859f23ea1a078ece644ea857
Danna <danna.wa@kakaobrain.com>
2022-07-11 23:47:44 +0900

    feat: 걷기, 뛰기 애니메이션 적용

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -7,12 +7,15 @@ public class Player : MonoBehaviour
     public float speed;
     private float hAxis;
     private float vAxis;
-    private Vector3 moveVec;
+    private bool walkDown;
     
-    // Start is called before the first frame update
-    void Start()
+    private Vector3 moveVec;
+
+    private Animator anim;
+    
+    void Awake()
     {
-        
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -21,6 +24,7 @@ public class Player : MonoBehaviour
         // "Horizontal" 값은 프로젝트 Input Settings에 정의되어 있음
         hAxis = Input.GetAxisRaw("Horizontal"); // horizon 값을 정수로 반환
         vAxis = Input.GetAxisRaw("Vertical");
+        walkDown = Input.GetButton("Walk"); // left shift 누르고 있을 때 walkDown을 true로 변경
 
         // 움직임 정의
         moveVec = new Vector3(hAxis, 0, vAxis).normalized; 
@@ -28,5 +32,8 @@ public class Player : MonoBehaviour
         // 대각선으로 움직일 때 속도가 빨라진다? 말이 안 됨 -> normalized로 해결
 
         transform.position += moveVec * speed * Time.deltaTime;
+        
+        anim.SetBool("isRun", moveVec != Vector3.zero); // 움직이지 않을 때 isRun이 false
+        anim.SetBool("isWalk", walkDown);
     }
 }
