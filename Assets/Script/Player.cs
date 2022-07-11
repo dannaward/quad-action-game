5f1a197461ff47ab022918529e798543bf0e3f53
Danna <danna.wa@kakaobrain.com>
2022-07-11 23:49:03 +0900

    feat: 걸을 때 속도 조절

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -31,7 +31,7 @@ public class Player : MonoBehaviour
         // 위, 왼쪽 같이 누르면 (1, 0, 1)인데, 이 경우 대각선은 2^(1/2)가 됨.
         // 대각선으로 움직일 때 속도가 빨라진다? 말이 안 됨 -> normalized로 해결
 
-        transform.position += moveVec * speed * Time.deltaTime;
+        transform.position += moveVec * speed * (walkDown ? 0.3f : 1f) * Time.deltaTime;
         
         anim.SetBool("isRun", moveVec != Vector3.zero); // 움직이지 않을 때 isRun이 false
         anim.SetBool("isWalk", walkDown);
